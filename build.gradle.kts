65b7858cec9eaf0d936db0ba28e294114af888ac
isalvama <inesalvans@gmail.com>
2026-04-16 20:22:13 +0200

    dependencies config

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,17 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.assertj:assertj-core:3.27.6")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+testing {
+    suites {
+        named<JvmTestSuite>("test") {
+            useJUnitJupiter("6.0.2")
+        }
+    }
 }
