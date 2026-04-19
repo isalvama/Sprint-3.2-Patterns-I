4b4d6e5c496d9e83f32537ddae48e71e0bd8d33d
isalvama <inesalvans@gmail.com>
2026-04-19 13:32:48 +0200

    feat: create ChinaPhoneValidator class with implementation of validatePhone() method, add chinaPhoneValidator attribute in ChinaPhoneNumberFormatter class and implement logic in formatPhone() method

--- src/main/java/second_level/format/formatter/concrete_formatters/ChinaPhoneNumberFormatter.java
+++ src/main/java/second_level/format/formatter/concrete_formatters/ChinaPhoneNumberFormatter.java
@@ -1,10 +1,19 @@
 package second_level.format.formatter.concrete_formatters;
 
+import second_level.format.ChinaPhoneValidator;
+import second_level.format.PhoneValidator;
 import second_level.format.formatter.abstract_formatter.PhoneNumberFormatter;
 
 public class ChinaPhoneNumberFormatter implements PhoneNumberFormatter {
+    private final PhoneValidator chinaPhoneValidator;
+
+    public ChinaPhoneNumberFormatter () {
+        this.chinaPhoneValidator = new ChinaPhoneValidator();
+    }
+
     @Override
-    public String formatPhone() {
-        return "";
+    public String formatPhone(long number) {
+        String numStr = chinaPhoneValidator.validatePhone(number);
+        return String.format("+86 %s %s %s", numStr.substring(0, 3), numStr.substring(3, 7), numStr.substring(7, 11));
     }
 }
