5c46626fc0c3a62053f6905a20f475bca1cdf44c
isalvama <inesalvans@gmail.com>
2026-04-22 11:14:02 +0200

    refactor: create Phone class and add it as parameter of formatPhone() method in PhoneNumberFormatter interface. Change the parameter type in all classes that implement PhoneNumberFormatter

---
+++ src/main/java/second_level/model/Phone.java
@@ -0,0 +1,13 @@
+package second_level.model;
+
+public class Phone {
+    private long number;
+
+    public Phone (long number) {
+        this.number = number;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+}
